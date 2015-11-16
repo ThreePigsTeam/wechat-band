e583847870b8a9896e752bd869c6a755459629d8
JayGuy <gaoxiandathss@gmail.com>
2015-11-16 19:16:26 +0800

    add doc and config.py

--- server.py
+++ server.py
@@ -4,29 +4,7 @@
 from flask import Flask, request, url_for, render_template
 from wechat_sdk import WechatBasic
 from database import *
-
-# configurations
-DATABASE = './tmp/band.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-localAddr = "59.66.139.102"
-appID = "wx77e762983e6c2463"
-appsecret = "96b447b1f7c4dbec926af2ab474edddc"
-token = "asdfasdf"
-
-ranklist = """<xml>
-    <ToUserName><![CDATA[%s]]></ToUserName>
-    <FromUserName><![CDATA[%s]]></FromUserName>
-    <CreateTime>123456789</CreateTime>
-    <MsgType><![CDATA[hardware]]></MsgType>
-    <HardWare>
-        <MessageView><![CDATA[myrank]]></MessageView>
-        <MessageAction><![CDATA[ranklist]]></MessageAction>
-    </HardWare>
-    <FuncFlag>0</FuncFlag>
-</xml>"""
+from config import *
 
 app = Flask(__name__)
 app.config.from_object(__name__)
